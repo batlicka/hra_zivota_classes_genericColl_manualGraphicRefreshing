d4b8e56b6989504f20ff6646bc69f649d713a710
batlicka <46314552+batlicka@users.noreply.github.com>
2019-04-28 17:47:21 +0200

    graphical game of life form

---
+++ GraphicalGameOfLifeForm/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace GraphicalGameOfLifeForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DrawAnotherGeneration = new System.Windows.Forms.Button();
+            this.LoadFromTxt = new System.Windows.Forms.Button();
+            this.SaveToTxt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // DrawAnotherGeneration
+            // 
+            this.DrawAnotherGeneration.Location = new System.Drawing.Point(579, 38);
+            this.DrawAnotherGeneration.Name = "DrawAnotherGeneration";
+            this.DrawAnotherGeneration.Size = new System.Drawing.Size(82, 23);
+            this.DrawAnotherGeneration.TabIndex = 0;
+            this.DrawAnotherGeneration.Text = "draw next gen";
+            this.DrawAnotherGeneration.UseVisualStyleBackColor = true;
+            this.DrawAnotherGeneration.Click += new System.EventHandler(this.DrawAnotherGeneration_Click);
+            // 
+            // LoadFromTxt
+            // 
+            this.LoadFromTxt.Location = new System.Drawing.Point(579, 67);
+            this.LoadFromTxt.Name = "LoadFromTxt";
+            this.LoadFromTxt.Size = new System.Drawing.Size(82, 23);
+            this.LoadFromTxt.TabIndex = 1;
+            this.LoadFromTxt.Text = "load from txt";
+            this.LoadFromTxt.UseVisualStyleBackColor = true;
+            this.LoadFromTxt.Click += new System.EventHandler(this.LoadFromTxt_Click);
+            // 
+            // SaveToTxt
+            // 
+            this.SaveToTxt.Location = new System.Drawing.Point(579, 96);
+            this.SaveToTxt.Name = "SaveToTxt";
+            this.SaveToTxt.Size = new System.Drawing.Size(82, 23);
+            this.SaveToTxt.TabIndex = 2;
+            this.SaveToTxt.Text = "save to txt";
+            this.SaveToTxt.UseVisualStyleBackColor = true;
+            this.SaveToTxt.Click += new System.EventHandler(this.SaveToTxt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 361);
+            this.Controls.Add(this.SaveToTxt);
+            this.Controls.Add(this.LoadFromTxt);
+            this.Controls.Add(this.DrawAnotherGeneration);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DrawAnotherGeneration;
+        private System.Windows.Forms.Button LoadFromTxt;
+        private System.Windows.Forms.Button SaveToTxt;
+    }
+}
+
---
+++ GraphicalGameOfLifeForm/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using vagunda_hra_zivota_classes_genericColl;
+
+namespace GraphicalGameOfLifeForm
+{
+    public partial class Form1 : Form
+    {
+        private int sideofField;
+        private int sideOfCell= 40; //výška strany buňky při čtvercovém hracím poli
+
+        private int HsideOfCell; //výška strany buňky, při nečtvercovém hracím poli
+        private int WsideOfCell; //šířka strany buňky, při nečtvercovém hracím poli
+        private bool DoPaint = false;
+
+        private Grid FieldOfLife;
+        public int Gen { get; set; }
+        private OpenFileDialog openFile = new OpenFileDialog();
+        private SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+        public Form1()
+        {
+            InitializeComponent();
+            Gen = 0;
+
+            openFile.Filter = "Text files (*.txt)|*.txt";
+            //openFile.InitialDirectory = @ "D:\"; 
+
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt";
+            saveFileDialog.RestoreDirectory = true;//If RestoreDirectory property is set to true that means the open file dialog box restores the current directory before closing.
+
+            if (this.Width < this.Height)
+                sideofField = this.Width;
+            else
+                sideofField = this.Height;            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DrawAnotherGeneration_Click(object sender, EventArgs e)
+        {
+            //aktualizuj + připrav další generaci
+            //aktualizuj paint
+            this.Invalidate();
+            FieldOfLife.PrepareAnotherGen();
+        }
+
+        private void LoadFromTxt_Click(object sender, EventArgs e)
+        {
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var filePath = openFile.FileName;
+                    FieldOfLife = new Grid(filePath);
+                    DoPaint = true;
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+
+            }
+        }
+
+        private void SaveToTxt_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //OpenFile()) != null
+                var filePath = saveFileDialog.FileName;
+                FieldOfLife.WriteToTXT(filePath);
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if(DoPaint != false)
+            {
+                
+
+                HsideOfCell = sideOfCell;//sideofField / (FieldOfLife.GetHeigh() - 2);
+                WsideOfCell = sideOfCell;//sideofField / (FieldOfLife.GetWidth() - 2);
+                // Create a local version of the graphics object    
+                Graphics g = e.Graphics;
+                //Graphics g = this.CreateGraphics();
+                Brush backGround = new SolidBrush(Color.Black);
+                Brush liveBrush = new SolidBrush(Color.Green);
+                Brush deadBrush = new SolidBrush(Color.Black);
+                Pen whitePen = new Pen(Color.White, 1);
+
+                g.FillRectangle(backGround, new Rectangle(0, 0, (FieldOfLife.GetWidth()-2)*sideOfCell, (FieldOfLife.GetWidth()*sideOfCell)-2));//širka, vyška
+
+
+                for (int i = 1; i < (FieldOfLife.GetHeigh() - 1); i++) //array.GetLength(0)=rows
+                {
+                    for (int j = 1; j < (FieldOfLife.GetWidth()- 1); j++)            //array.GetLength(1)= colums
+                    {
+                        if (FieldOfLife.currArray[i, j].IsAlive == true)
+                        {
+                            g.FillRectangle(liveBrush, new Rectangle((j - 1) * HsideOfCell, (i - 1) * WsideOfCell, HsideOfCell, WsideOfCell));
+                            g.DrawRectangle(whitePen, new Rectangle((j - 1) * HsideOfCell, (i - 1) * WsideOfCell, HsideOfCell, WsideOfCell));
+                        }
+                        else
+                        {
+                            //g.FillRectangle(deadBrush, new Rectangle((i - 1) * sideOfCell, (j - 1) * sideOfCell, sideOfCell, sideOfCell));
+                            g.DrawRectangle(whitePen, new Rectangle((j - 1) * HsideOfCell, (i - 1) * WsideOfCell, HsideOfCell, WsideOfCell));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
